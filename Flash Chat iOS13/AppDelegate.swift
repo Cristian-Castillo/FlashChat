0901fb2551bbb60f0937bb352d08c350911e30b2
Cristian C. Castillo <cris.c.castillo1@gmail.com>
2021-04-06 20:32:03 -0700

    Update AppDelegate.swift

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -1,9 +1,7 @@
 //
 //  AppDelegate.swift
 //  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Cristian C. Castillo
 //
 
 import UIKit
